386c37a217b313763c93120183b4395b8c99532a
tsimsekburgan <TSimsek@burgan.com.tr>
2023-03-08 15:14:21 +0300

    delete migrate files

--- amorphie.workflow.zeebe/Program.cs
+++ amorphie.workflow.zeebe/Program.cs
@@ -1,3 +1,4 @@
+using amorphie.core.security.Extensions;
 using Dapr.Client;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,17 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 var daprClient = new DaprClientBuilder().Build();
 
-builder.Services.AddDbContext<WorkflowDBContext>
-    ((options) =>
-    {
-        options.UseNpgsql("Host=localhost:5432;Database=workflow;Username=postgres;Password=postgres");
-    });
-
+// builder.Services.AddDbContext<WorkflowDBContext>
+//     ((options) =>
+//     {
+//         options.UseNpgsql("Host=localhost:5432;Database=workflow;Username=postgres;Password=postgres");
+//     });
+await builder.Configuration.AddVaultSecrets("workflow-secretstore",new string[]{"workflow-secretstore"});
+var postgreSql = builder.Configuration["workflowdb"];
 builder.Services.AddDaprClient();
 builder.Logging.ClearProviders();
 builder.Logging.AddJsonConsole();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<WorkflowDBContext>
+    (options => options.UseNpgsql(postgreSql, b => b.MigrationsAssembly("amorphie.workflow.data")));
 
 var app = builder.Build();
 
