368677ae801ee5c7338d25795247ae2798576254
tsimsekburgan <TSimsek@burgan.com.tr>
2023-03-08 14:40:41 +0300

    workflow-secretstore

--- amorphie.workflow/Program.cs
+++ amorphie.workflow/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.OpenApi.Models;
 //using SecretExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
-await builder.Configuration.AddVaultSecrets("workflow-secretstore",new string[]{"DatabaseConnections"});
+await builder.Configuration.AddVaultSecrets("workflow-secretstore",new string[]{"workflow-secretstore"});
 var postgreSql = builder.Configuration["workflowdb"];
 
 
