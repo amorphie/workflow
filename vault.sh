63402ff29d60613e88c2b63adf10b2813608ccc1
mt <mehmet.tosun@outlook.com>
2023-05-29 11:31:42 +0300

    Added docker mock service

--- vault.sh
+++ vault.sh
@@ -1,3 +1,5 @@
 sleep 5 &&
 
 curl -X POST 'http://vault:8200/v1/secret/data/amorphie-secretstore' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"workflowdb":"Host=localhost:5432;Database=workflow;Username=postgres;Password=postgres;Include Error Detail=true;"} }'
+&& sleep 5 &&
+curl -X POST 'http://localhost:8082/mocks' -H "Content-Type: application/x-yaml" --data-binary "@helloworld.yml"
