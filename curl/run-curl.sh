8337e0dca1a92399906d8ba417e6305e24560caa
Mehmet TOSUN <93265833+middt@users.noreply.github.com>
2023-05-29 13:43:53 +0300

    Added curl

--- curl/run-curl.sh
+++ curl/run-curl.sh
@@ -1,5 +1,7 @@
 sleep 5 &&
 
 curl -X POST 'http://vault:8200/v1/secret/data/amorphie-secretstore' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"workflowdb":"Host=localhost:5432;Database=workflow;Username=postgres;Password=postgres;Include Error Detail=true;"} }'
-&& sleep 5 &&
-curl -X POST 'http://localhost:8082/mocks' -H "Content-Type: application/x-yaml" --data-binary "@helloworld.yml"
+
+curl -XPOST 'http://mock-service:8081/mocks' -H "Content-Type: application/x-yaml" --data-binary "@./usr/local/bin/helloworld.yml"
+
+
