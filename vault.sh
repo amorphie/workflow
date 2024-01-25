1c7b377276914cd634bfd65d6c3246e2e949cc8b
ikarakayali <129904470+ikarakayali@users.noreply.github.com>
2024-01-25 14:45:37 +0300

    Update vault.sh

--- vault.sh
+++ vault.sh
@@ -1,11 +1,3 @@
-curl -X POST 'http://vault:8200/v1/secret/data/workflow-secretstore' -H "Content-Type: application/json" -H "X-Vault-Token: admin"  -d '{ 
-    "data": {
-        "workflowdb":"Host=localhost:5432;Database=workflow;Username=postgres;Password=postgres;Include Error Detail=true;",
-        "postgresql":"Host=localhost:5432;Database=users;Username=postgres;Password=postgres",
-        "templateEngineUrl":"https://test-template-engine.burgan.com.tr/Template/Render",
-        "templateEngine":"https://test-template-engine.burgan.com.tr",
-        "messagingGatewayUrl":"https://test-messaginggateway.burgan.com.tr/api/v2/Messaging/sms/message",
-        "hubUrl":"amorphie-workflow-hub.test-amorphie-workflow-hub",
-        "redisEndPoints" : "localhost:6379"
-        }
-    }'
+sleep 5 &&
+
+curl -X POST 'http://vault:8200/v1/secret/data/workflow-secretstore' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"workflowdb":"Host=localhost:5432;Database=workflow;Username=postgres;Password=postgres;Include Error Detail=true;"} }'
