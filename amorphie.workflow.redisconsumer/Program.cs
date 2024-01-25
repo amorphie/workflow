b8db1a3885a827c8d3351cc1427d677b4585f27f
ikarakayali <ibrahimkarakayali88@gmail.com>
2024-01-25 13:49:25 +0300

    program.cs formatted

--- amorphie.workflow.redisconsumer/Program.cs
+++ amorphie.workflow.redisconsumer/Program.cs
@@ -24,14 +24,6 @@ internal class Program
         var redisEndPoint = conf.GetValue<string>("redisEndPoints");
         StateHelper.HubUrl = conf.GetValue<string>("hubUrl");
 
-        //DI setup
-        //IServiceCollection services = new ServiceCollection();
-        //services.AddDbContext<WorkflowDBContext>(options => options.UseNpgsql(postgreSql, b => b.MigrationsAssembly("amorphie.workflow.data")));
-        //var serviceProvider = services.BuildServiceProvider();
-
-
-        //var dbContext = serviceProvider.GetRequiredService<WorkflowDBContext>();
-
         var configurationOptions = new ConfigurationOptions
         {
             EndPoints =
@@ -39,8 +31,6 @@ internal class Program
                 redisEndPoint
             }
         };
-        //var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(configurationOptions);
-        //var redisDb = connectionMultiplexer.GetDatabase();
         var tokenSource = new CancellationTokenSource();
         var cancellationToken = tokenSource.Token;
 
@@ -61,59 +51,7 @@ internal class Program
         .Build();
 
         host.Run();
-        // var streamCleaner = new StreamCleaner();
-        // var tasks = new List<Task>
-        // {
 
-        //     Task.Run(() => ActivateExporters(dbContext, redisDb, consumerName, readingStrategy, cancellationToken)),
-        //     Task.Run(()=>streamCleaner.TrimNotAttachedStream(redisDb, 100, cancellationToken)),
-
-        // };
-        // Task tasksResult;
-        // try
-        // {
-        //     tasksResult = await Task.WhenAny(tasks);
-
-        // }
-        // catch (System.Exception)
-        // {
-
-        //     throw;
-        // }
-        // Console.WriteLine(tasksResult?.Exception?.Message);
-        // Console.WriteLine(tasksResult?.Exception?.InnerException?.Message);
-
-        // //await ActivateExporters(dbContext, redisDb, consumerName, readingStrategy, cancellationToken);
-
-        // tokenSource.Cancel();
-
-
-    }
-
-    public static async Task ActivateExporters(WorkflowDBContext dbContext, IDatabase redisDb, string consumerName, string readingStrategy, CancellationToken cancellationToken)
-    {
-        var deploymentExporter = new MessageStartEventSubscriptionExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var incidentExporter = new IncidentExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var messageExporter = new MessageExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var messageStartEventExporter = new MessageStartEventSubscriptionExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var messageSubscriptionExporter = new MessageSubscriptionExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var processInstanceExporter = new ProcessInstanceExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var variableExporter = new VariableExporter(dbContext, redisDb, consumerName, readingStrategy);
-        var jobExporter = new JobExporter(dbContext, redisDb, consumerName, readingStrategy);
-
-        while (!cancellationToken.IsCancellationRequested)
-        {
-            await deploymentExporter.Attach(cancellationToken);
-            await incidentExporter.Attach(cancellationToken);
-            await messageStartEventExporter.Attach(cancellationToken);
-            await messageSubscriptionExporter.Attach(cancellationToken);
-            await messageExporter.Attach(cancellationToken);
-            await processInstanceExporter.Attach(cancellationToken);
-            await variableExporter.Attach(cancellationToken);
-            await jobExporter.Attach(cancellationToken);
-            await Task.Delay(1000);
-
-        }
     }
 
 }
