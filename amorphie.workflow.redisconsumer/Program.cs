4db4c4078c7faf13ffb96ca033326825c1931fec
ikarakayali <ibrahimkarakayali88@gmail.com>
2024-02-19 11:52:08 +0300

    store name changed

--- amorphie.workflow.redisconsumer/Program.cs
+++ amorphie.workflow.redisconsumer/Program.cs
@@ -22,7 +22,7 @@ internal class Program
         var configurationBuilder = new ConfigurationBuilder();
         var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
         //configurationBuilder.AddJsonFile($"appsettings.{environment}.json", false, true);
-        await configurationBuilder.AddVaultSecrets("workflow-secretstore", new[] { "workflow-secretstore" });
+        await configurationBuilder.AddVaultSecrets("redisconsumer-secretstore", new[] { "redisconsumer-secretstore" });
         var conf = configurationBuilder.Build();
         var postgreSql = conf.GetValue<string>("workflowdb");
         var redisEndPoint = conf.GetValue<string>("redisEndPoints");
