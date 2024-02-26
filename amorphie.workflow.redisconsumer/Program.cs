5d56e7e383eea3352696dfc479fe4441fe2a1e76
ikarakayali <ibrahimkarakayali88@gmail.com>
2024-02-26 15:06:53 +0300

    test dapr

--- amorphie.workflow.redisconsumer/Program.cs
+++ amorphie.workflow.redisconsumer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using amorphie.core.Extension;
 using amorphie.workflow.redisconsumer;
+using Dapr.Client;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using StackExchange.Redis;
@@ -9,61 +10,74 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-
-        //ConfigurationBuilder setup
         var configurationBuilder = new ConfigurationBuilder();
-        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        //configurationBuilder.AddJsonFile($"appsettings.{environment}.json", false, true);
-        await configurationBuilder.AddVaultSecrets("workflow-secretstore", new[] { "workflow-secretstore" });
-        var conf = configurationBuilder.Build();
-        var postgreSql = conf.GetValue<string>("workflowdb");
-        var redisEndPoint = conf.GetValue<string>("redisEndPoints");
-        StateHelper.HubUrl = conf.GetValue<string>("hubUrl");
-
-        var configurationOptions = new ConfigurationOptions
-        {
-            EndPoints =
-            {
-                redisEndPoint
-            }
-        };
-        var tokenSource = new CancellationTokenSource();
-        var cancellationToken = tokenSource.Token;
-        Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(conf).CreateLogger();
-        Log.Information($"Amorphie Workflow Redis Consumer Starting : {DateTime.Now}");
-
         try
         {
+            DaprClient daprClient = new DaprClientBuilder().Build();
 
+            configurationBuilder.AddInMemoryCollection(await daprClient.GetSecretAsync("workflow-secretstore", "workflow-secretstore"));
+            var conf = configurationBuilder.Build();
 
-            IHost host = Host.CreateDefaultBuilder(args)
-            .ConfigureServices(services =>
-            {
-
-                services.AddDaprClient();
-                services.AddScoped<IDatabase>(cfg =>
-                {
-                    IConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(configurationOptions);
-                    return multiplexer.GetDatabase();
-                });
-                services.AddDbContext<WorkflowDBContext>(options => options.UseNpgsql(postgreSql, b => b.MigrationsAssembly("amorphie.workflow.data")));
-                services.AddHostedService<BulkReadWorker>();
-                //services.AddHostedService<ExporterWorker>();
-                services.AddHostedService<StreamCleanerWorker>();
-            })
-            .UseSerilog(Log.Logger, true)
-            .Build();
-
-            host.Run();
+            var postgreSql = conf.GetValue<string>("workflowdb");
+            Console.WriteLine("postgres connstr : ", postgreSql);
         }
         catch (Exception ex)
         {
-            Log.Fatal("An unhandled exception occured while starting", ex);
+            throw new Exception("An Error Occured At Dapr Secret Store. Detail:" + ex);
         }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
+        // //ConfigurationBuilder setup
+        // var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        // //configurationBuilder.AddJsonFile($"appsettings.{environment}.json", false, true);
+        // await configurationBuilder.AddVaultSecrets("workflow-secretstore", new[] { "workflow-secretstore" });
+        // var conf = configurationBuilder.Build();
+        // var postgreSql = conf.GetValue<string>("workflowdb");
+        // var redisEndPoint = conf.GetValue<string>("redisEndPoints");
+        // StateHelper.HubUrl = conf.GetValue<string>("hubUrl");
+
+        // var configurationOptions = new ConfigurationOptions
+        // {
+        //     EndPoints =
+        //     {
+        //         redisEndPoint
+        //     }
+        // };
+        // var tokenSource = new CancellationTokenSource();
+        // var cancellationToken = tokenSource.Token;
+        // Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(conf).CreateLogger();
+        // Log.Information($"Amorphie Workflow Redis Consumer Starting : {DateTime.Now}");
+
+        // try
+        // {
+
+
+        //     IHost host = Host.CreateDefaultBuilder(args)
+        //     .ConfigureServices(services =>
+        //     {
+
+        //         services.AddDaprClient();
+        //         services.AddScoped<IDatabase>(cfg =>
+        //         {
+        //             IConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect(configurationOptions);
+        //             return multiplexer.GetDatabase();
+        //         });
+        //         services.AddDbContext<WorkflowDBContext>(options => options.UseNpgsql(postgreSql, b => b.MigrationsAssembly("amorphie.workflow.data")));
+        //         services.AddHostedService<BulkReadWorker>();
+        //         //services.AddHostedService<ExporterWorker>();
+        //         services.AddHostedService<StreamCleanerWorker>();
+        //     })
+        //     .UseSerilog(Log.Logger, true)
+        //     .Build();
+
+        //     host.Run();
+        // }
+        // catch (Exception ex)
+        // {
+        //     Log.Fatal("An unhandled exception occured while starting", ex);
+        // }
+        // finally
+        // {
+        //     Log.CloseAndFlush();
+        // }
 
     }
 
