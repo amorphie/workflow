8b1f2cf5a5d51af68f6d342efdae02199b7ffb9d
ikarakayali <ibrahimkarakayali88@gmail.com>
2024-04-04 15:12:02 +0300

    end point fix

--- amorphie.workflow/Modules/DefinitionV2.cs
+++ amorphie.workflow/Modules/DefinitionV2.cs
@@ -9,29 +9,16 @@ public static class DefinitionV2Module
     public static void MapDefinitionV2Endpoints(this WebApplication app)
     {
 
-        app.MapPost("/workflow/saveWorkflow/new", SaveWorkflowAsync)
+        app.MapPost("/workflow/saveWorkflow", SaveWorkflowAsync)
             .Produces<PostWorkflowDefinitionResponse>(StatusCodes.Status200OK)
             .Produces(StatusCodes.Status201Created)
             .WithOpenApi(operation =>
               {
-                  operation.Summary = "Saves or updates workflow definition in new style.";
+                  operation.Summary = "Saves or updates workflow definition. if NewStates object is not null then it commits states&transition in new style else legacy style.";
+                  operation.Description="If NewStates and States object is given in same request then NewStates processed";
                   operation.Tags = new List<OpenApiTag> { new() { Name = "V2 Definition" } };
 
-                  operation.Responses["200"] = new OpenApiResponse { Description = "Definition updated." };
-                  operation.Responses["201"] = new OpenApiResponse { Description = "New definition created." };
-
-                  return operation;
-              });
-
-        app.MapPost("/workflow/saveWorkflow/legacy", SaveWorkflowLegacyAsync)
-            .Produces<PostWorkflowDefinitionResponse>(StatusCodes.Status200OK)
-            .Produces(StatusCodes.Status201Created)
-            .WithOpenApi(operation =>
-              {
-                  operation.Summary = "Saves or updates workflow and its states and transitions definition in legacy style.";
-                  operation.Tags = new List<OpenApiTag> { new() { Name = "V2 Definition" } };
-                  operation.Responses["200"] = new OpenApiResponse { Description = "Definition updated." };
-                  operation.Responses["201"] = new OpenApiResponse { Description = "New definition created." };
+                  operation.Responses["200"] = new OpenApiResponse { Description = "Definition saved." };
                   return operation;
               });
     }
@@ -43,12 +30,6 @@ public static class DefinitionV2Module
       )
     {
         var response = await service.SaveAsync(data);
-        return ApiResult.CreateResult(response);
-    }
-
-    static async Task<IResult> SaveWorkflowLegacyAsync([FromServices] TransferService transferService, [FromBody] WorkflowCreateDto data, CancellationToken cancellationToken)
-    {
-        var response = await transferService.SaveDefinitionToLegacyBulkAsync(data, cancellationToken);
         return ApiResult.CreateResult(response);
     }
 
